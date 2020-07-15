449e1fbc559431d2d6751f630139e5fca5ddd343
ibrazbib27 <ibrahim.zbib@macys.com>
2020-07-14 23:47:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 $(document).ready(function () {
 $("#yellow-block").remove();
 $("#keyboard-upper-container").hide();
-let startTime, endTime, t = 0, i, key, numMistakes = 0, n = 0, m = 0, space = 0, feedback = "", sent = "";
+let startTime, endTime, i, key, numMistakes = 0, n = 0, m = 0, space = 0, feedback = "", sent = "";
+let over = false;
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
     
 $("#sentence").html('<mark>'+(sentences[m]).substr(n,n+1)+'</mark>'+(sentences[m]).substr(n+1));  
@@ -10,83 +11,85 @@ $("#target-letter").html((sentences[m][n]));
 
                  
               
-
-          $(this).on('keypress',function(e) {
+$(this).on('keypress',function(e) {
               
-              if(n === 0 && m === 0)
-                startTime =  new Date();
-              
-        key = e.which;
+    if(n === 0 && m === 0)
+        startTime =  new Date();
+    key = e.which;
            
-       $( ".well.well-lg.key" ).each(function( ) {
-  if($( this).attr("id") == e.which){
-      i = $( this).attr("id");
-      $("#"+i).css("background-color", "yellow");
+    $( ".well.well-lg.key" ).each(function( ) {
+        if($( this).attr("id") == e.which){
+            i = $( this).attr("id");
+        $("#"+i).css("background-color", "rgb(255, 255, 0)");
   }
 });
 
-   
-                  
-                     
-                      if($("#"+key).text()=== sentences[m][n]){
-                          feedback += "<icon class='glyphicon glyphicon-ok bg-success'></icon>";
-                          $("#feedback").html(feedback);
-                           n++;
-                      
-                          
-                           
-                      }
-                
-                    else if(" " === sentences[m][n]){
-                          feedback += "<icon class='glyphicon glyphicon-ok bg-success'></icon>";
-                          $("#feedback").html(feedback);
-                        space++;
-                      n++;
-                         
-                          
-                      }
-                       else{
-                           feedback +="<icon class='glyphicon glyphicon-remove bg-danger'></icon>"
-                           $("#feedback").html(feedback);
-                           n++;
-                           numMistakes++;
-                            
-                           
-                       }   
-                   
+     if(over === false ){
+        switch (sentences[m][n]){
+            case $(`#${e.which}`).text():
+                feedback += "<icon class='glyphicon glyphicon-ok bg-success'></icon>";
+                $("#feedback").html(feedback);
+                e.which
+                n++;
+                break;
+             case " ":
+                if($(`#${key}`).text() === "Space")
+                    feedback += "<icon class='glyphicon glyphicon-ok bg-success'></icon>";
+                else
+                    feedback +="<icon class='glyphicon glyphicon-remove bg-danger'></icon>";
                         
-                        if(n === sentences[m].length){
+                $("#feedback").html(feedback);
+                space++;
+                n++;
+                break;
+             default:
+                feedback +="<icon class='glyphicon glyphicon-remove bg-danger'></icon>";
+                $("#feedback").html(feedback);
+                n++;
+                numMistakes++;
+                break;      
+        }
+              //moving the yellow block and changing target character
+        $("#target-letter").html(sentences[m][n]);
+        sent = (sentences[m]).substr(0,n)+'<mark>'+sentences[m][n]+'</mark>'+(sentences[m]).substr(n+1);
+        $("#sentence").html(sent);
+    }
+     if(n === sentences[m].length){
                             n = 0;
                             px = 0;
                             m++;
-                            if(m < 5){
+                        if(m === 5){
+                            over = true;
+                            //m truns to 4 in order to not get any undefined errors for sentances array
+                            m = 4;
+                        }
+                            
+                        
+                        if(over === false){
                             feedback="";
                             $("#feedback").html(feedback);
                             $("#sentence").html(sentences[m]); 
-                            }
-                            else{
-                        endTime = new Date();
-                        let elapseTime = endTime - startTime;
-                        space = space + 5;
-                        elapseTime /= 1000;
-                       let mins = (elapseTime/60).toFixed(2);
-                        let results = (((space / mins) - (2 * numMistakes)).toFixed(2))
-                        let msg = "Your score for this round was: " + results + " (words/min)";
-                                $( "#sentence" ).html(msg);
-                                feedback="";
+                        }
+                        else{
+                            n = n-1;
+                            endTime = new Date();
+                            let elapseTime = endTime - startTime;
+                            space = space + 5;
+                            elapseTime /= 1000;
+                            let mins = (elapseTime/60).toFixed(2);
+                            let results = (((space / mins) - (2 * numMistakes)).toFixed(2))
+                            let msg = "Your score for this round was: " + results + " (words/min)";
+                            $( "#sentence" ).html(msg);
+                            feedback="";
                             $("#feedback").html(feedback);
-                                 $("#target-letter").html('<button class="bg bg-success">Play Again?<button>');
+                            $("#target-letter").html('<button class="bg bg-success">Play Again?<button>');
                             $("button").on("click", function(e) {
                                  location.reload(true);
                             });
        
                             }
   
-                        }
-               $("#target-letter").html(sentences[m][n]);
-                sent = (sentences[m]).substr(0,n)+'<mark>'+sentences[m][n]+'</mark>'+(sentences[m]).substr(n+1);
-             
-              $("#sentence").html(sent);   
+                     }
        
 });
  
@@ -107,109 +110,35 @@ $("#target-letter").html((sentences[m][n]));
    /*
    'if' statement deals with toggling upper case keyboard when shift key is released (shift key functionality). All 'else if' statements deal with highlighting and unhighlighting special key charcaters except the space keyon the keyboard. The 'else' statement deals with highlighting and unhighlighting alpha-numeric keys and the space key on the keyboard.
    */   
-  if(e.which === 16){
-             $("#keyboard-upper-container").hide();
-             $("#keyboard-lower-container").show();
-     }
-      else if(192 == e.which) {
-            
-              $("#126").css("background-color", "#f5f5f5");
-             $("#96").css("background-color", "#f5f5f5");
-             
-         } 
-     else if(189 === e.which) {
-              $("#45").css("background-color", "#f5f5f5");
-             $("#95").css("background-color", "#f5f5f5");
-             
-         } 
-         else if(187 === e.which) {
-              $("#43").css("background-color", "#f5f5f5");
-             $("#61").css("background-color", "#f5f5f5");
-             
-         } 
-         else if(220 === e.which) {
-              $("#92").css("background-color", "#f5f5f5");
-             $("#124").css("background-color", "#f5f5f5");
-             
-         } 
-         else if(221 === e.which) {
-              $("#93").css("background-color", "#f5f5f5");
-             $("#125").css("background-color", "#f5f5f5");
-             
-         } 
-       else if(219 === e.which) {
-              $("#123").css("background-color", "#f5f5f5");
-             $("#91").css("background-color", "#f5f5f5");
-             
-         }
-         else if(222 === e.which) {
-              $("#39").css("background-color", "#f5f5f5");
-             $("#34").css("background-color", "#f5f5f5");
-             
-         }
-       else if(186 === e.which) {
-              $("#59").css("background-color", "#f5f5f5");
-             $("#58").css("background-color", "#f5f5f5");
-             
-         } 
-      else if(191 === e.which) {
-              $("#47").css("background-color", "#f5f5f5");
-             $("#63").css("background-color", "#f5f5f5");
-             
-         }
-          else if(190 === e.which) {
-              $("#46").css("background-color", "#f5f5f5");
-             $("#62").css("background-color", "#f5f5f5");
-             
-         }
-     else if(188 === e.which) {
-              $("#44").css("background-color", "#f5f5f5");
-             $("#60").css("background-color", "#f5f5f5");
-             
-         }
-    else{
-       
-        let index1 = -1, index2 = -1;
-       
-        if(($("#"+e.which).css( "background-color" )) == "rgb(255, 255, 0)" ||($("#"+e.which).css( "background-color" )) == "rgb(245, 245, 245)"){
-       $.each(($('#keyboard-upper-container').find("span")), function(x, valz) {
-          
-           if(($(this).attr("id")) == e.which){
-               index1 = x;
-      
-           }
-  
-});
-     $.each(($('#keyboard-lower-container').find("span")), function(x, valz) {
-          
-           if(($(this).attr("id")) == e.which){
-               index2 = x;
-           ;
-           }
-  
-});
+switch (e.which){
+    case 16:
+        $("#keyboard-upper-container").hide();
+        $("#keyboard-lower-container").show();
+    case 32:
+        $("#32").css("background-color", "#f5f5f5");
+    default:
+       $.each(($('#keyboard-upper-container').find("span")), function(x) {
+           if((($('#keyboard-upper-container span').eq(x)).css( "background-color" )) === "rgb(255, 255, 0)")
+                undoUpperCaseHighlight(x);
+       });
+       $.each(($('#keyboard-lower-container').find("span")), function(x) { 
+          if((($('#keyboard-lower-container span').eq(x)).css( "background-color" )) === "rgb(255, 255, 0)")
+                undoLowerCaseHighlight(x); 
+        });
     
-        if(index1 !== -1) {
-            
-        ($('#keyboard-lower-container span').eq(index1)).css("background-color", "#f5f5f5");
-           
-            index1 = -1;
+        function undoUpperCaseHighlight(index){
+            ($('#keyboard-upper-container span').eq(index)).css("background-color", "#f5f5f5");
         }
-         if(index2 !== -1) {
-              ($('#keyboard-upper-container span').eq(index2)).css("background-color", "#f5f5f5");
-             
-            index2 = -1;
-         } 
-           
-         
-       
-       
-        $("#"+e.which).css("background-color", "#f5f5f5");
         
-        }
+        function undoLowerCaseHighlight(index){
+             ($('#keyboard-lower-container span').eq(index)).css("background-color", "#f5f5f5");
+         }
+           
+        
+        
        
+    
     }
-        
         
 });
     
